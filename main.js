f7af43a4705336ff2db9b663d696f79e86bca4c5
Amrut Gotpagar <amrutgotpagar@gmail.com>
2024-07-05 22:03:49 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+
+const template = document.querySelector("#pet-card-template")
+const wrapper = document.createDocumentFragment()
+
+async function start() {
+    const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
+    const weatherData = await weatherPromise.json()
+
+    const ourTemperature = weatherData.properties.periods[0].temperature
+    document.querySelector("#temperature-output").textContent = ourTemperature
+}
+
+start()
+
+async function petsArea() {
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsData = await petsPromise.json()
+    petsData.forEach(pet => {
+        const clone = template.content.cloneNode(true)
+
+        clone.querySelector("h3").textContent = pet.name
+        clone.querySelector(".pet-description").textContent = pet.description
+        clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+
+        if (!pet.photo) pet.photo = "images/Fallback.jpg"
+        clone.querySelector(".pet-card-photo img").src = pet.photo
+        clone.querySelector(".pet-card-photo img").alt = 'A ${pet.species} named ${pet.name}'
+        wrapper.appendChild(clone)
+    })
+
+    document.querySelector(".list-of-pets").appendChild(wrapper)
+}
+
+petsArea()
+
+function createAgeText(birthYear) {
+    const currentYear = new Date().getFullYear()
+    const age = currentYear - birthYear
+
+    if (age == 1) return "1 year old "
+    if (age == 0) return "Less tahn a year old"
+
+    return age + " years old "
+}
